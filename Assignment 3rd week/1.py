daa133146627eb2648c07f91fb6beb3c128a0d6e
সাদমান সৌমিক শাওন <81482873+Shadmans-World@users.noreply.github.com>
2021-07-04 14:24:37 +0600

    Delete Assignment 3rd week directory

--- Assignment 3rd week/1.py
+++
@@ -1,9 +0,0 @@
-#1. Write a Python program to get a string made of the first 2 and the last 2 chars from a given a string. 
-#If the string length is less than 2, return instead of the empty string.
-a = input()
-
-if len(a)>2:
-    print(a[0:2]+ a[-2:])
-else:
-    print("Empty String")
-  
